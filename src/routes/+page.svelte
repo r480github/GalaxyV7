d861096d9e3005015eb6c5d1f6a376fc56fe9fa7
rogo! <r480github@gmail.com>
2026-04-18 00:52:47 -0400

    feat(landing): entrance animation and custom fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import '$lib/style/main.css';
 	import { browser } from '$app/environment';
 	import gsap from 'gsap';
+	import { onMount } from 'svelte';
 	let selected = $state(null);
 	let mode = $state(browser ? localStorage.getItem('mode') : null);
 
@@ -13,21 +14,55 @@
 		gsap.to('.stagger', {
 			y: -50,
 			opacity: 0,
-			duration: 0.3,
+			duration: 0.2,
 			stagger: 0.04,
 			onComplete: () => {
 				mode = selected;
 			}
 		});
 	}
+	onMount(() => {
+		gsap.fromTo(
+			'.question',
+			{
+				y: 50,
+				opacity: 0
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 0.4,
+				onComplete: () => {   
+					gsap.fromTo(
+						'.option',
+						{
+							y: 50,
+							opacity: 0
+						},
+						{
+							y: 0,
+							opacity: 1,
+							duration: 0.3,
+							stagger: 0.08
+						}
+					);
+				}
+			}
+		);
+	});
 </script>
 
 {#if mode == null}
 	<div class="container">
-		<h1 class="stagger">How would you like to use Galaxy?</h1>
+		<h1 class="stagger question">How would you like to use Galaxy?</h1>
 
 		<div class="options">
-			<button class="option stagger" class:active={selected === 'os'} onclick={() => choose('os')}>
+			<button
+				class="option stagger"
+				class:active={selected === 'os'}
+				onclick={() => choose('os')}
+        style="opacity:0;"
+			>
 				<h2>OS Mode</h2>
 			</button>
 
@@ -35,13 +70,14 @@
 				class="option stagger"
 				class:active={selected === 'website'}
 				onclick={() => choose('website')}
+        style="opacity:0;"
 			>
 				<h2>Website Mode</h2>
 			</button>
 		</div>
 
 		{#if selected}
-			<button class="continue stagger" onclick={() => next()}> Continue </button>
+			<button class="continue stagger" onclick={() => next()}> <p>Continue</p> </button>
 		{/if}
 	</div>
 {:else if mode === 'os'}
