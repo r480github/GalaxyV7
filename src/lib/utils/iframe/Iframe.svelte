b30ab8007d1214b8f59c29dab229d4f962cb69f0
rogo! <r480github@gmail.com>
2026-04-23 23:06:23 -0400

    feat(proxy): add /prox route and Iframe wrapper component

---
+++ src/lib/utils/iframe/Iframe.svelte
@@ -0,0 +1,26 @@
+<script>
+	let { url = '', type, id } = $props();
+</script>
+
+<input id="url" type="text" class="search-input search" placeholder="Search" value={url} />
+<input id="prxType" value={type} />
+<iframe id="iframe"></iframe>
+
+<svelte:head>
+	<script src="/baremux/index.js" defer></script>
+	<script src="/uv/uv.bundle.js" defer></script>
+	<script src="/uv/uv.config.js" defer></script>
+	<script src="/scram/scramjet.all.js" defer></script>
+	<script src="/stuff.js" defer></script>
+</svelte:head>
+
+<style>
+iframe {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+input {
+		display: none;
+	}
+</style>
---
+++ src/routes/prox/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Frame from '$lib/utils/iframe/Iframe.svelte';
+</script>
+
+<div class="nav">
+	<div class="left"></div>
+	<div class="middle">
+		<input id="search" placeholder="Search" />
+	</div>
+	<div class="right"></div>
+</div>
+<Frame url="example.net" type="default" id="1" />
+
+<style>
+	:global(body) {
+		height: 100vh;
+		margin: 0;
+		overflow: hidden;
+	}
+</style>
