f2d76c99eac82fcecda151b89e48cd4606e0ad20
rogo! <r480github@gmail.com>
2026-06-04 23:27:14 -0400

    fix(proxy): temporarily disable refulx for libcurl

--- src/lib/utils/browser/iframe.svelte
+++ src/lib/utils/browser/iframe.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { activeTab, reloadSignal, goBackSignal, goForwardSignal } from '$lib/stores/index.js';
 	import { getOriginalUrl } from '$lib/lethe/decode';
-
 	let { id, src = null, onnavigate } = $props();
 	let frame;
 
@@ -39,7 +38,8 @@
 		if ($goForwardSignal === id) {
 			try {
 				frame.contentWindow.history.forward();
-			} catch (e) {}
+			} catch (e) {
+			}
 			$goForwardSignal = null;
 		}
 		if ($reloadSignal === id) {
