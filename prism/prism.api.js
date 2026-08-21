b440536d98361f2d16756ff20b7e38433b9771c1
rogo! <r480github@gmail.com>
2026-08-20 21:36:40 -0700

    feat(scramjet): remove tilda

--- prism/prism.api.js
+++ prism/prism.api.js
@@ -342,7 +342,7 @@ var $scramjetController;
 			n = r(286),
 			a = r(355);
 		let c = {
-				prefix: '/~/prism/',
+				prefix: '/prism/',
 				scramjetPath: '/scramjet/scramjet.js',
 				injectPath: '/controller/controller.inject.js',
 				wasmPath: '/scramjet/scramjet.wasm',
